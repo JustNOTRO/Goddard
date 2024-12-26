7f87622300a7e1cd12f8220e4c2ebd583658eaf4
notro <eilonhafzadi7311@gmail.com>
2024-12-26 23:22:44 +0200

    [action] Improve OpenIssueAction

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,14 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
 
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+
+    // define any required OkHttp artifacts without version
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+
+    implementation("org.json:json:20171018")
+
     intellijPlatform {
         val version = providers.gradleProperty("platformVersion")
         val type = providers.gradleProperty("platformType")
--- src/main/kotlin/dev/notro/ideissuer/OpenIssueAction.kt
+++ src/main/kotlin/dev/notro/ideissuer/OpenIssueAction.kt
@@ -1,50 +1,120 @@
 package dev.notro.ideissuer
 
-import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.TextRange
-import java.net.HttpURLConnection
-import java.net.URL
-
-typealias GitRemoteUrl = String
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 
 private const val IDE_ISSUER_TITLE = "IDE-Issuer"
+
 private const val ISSUE_TITLE_MAX_LENGTH = 255
+
+private val JSON = "application/json".toMediaType()
 
 class OpenIssueAction : AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project ?: return
 
-        var title: String? = null
+        var title: String? = Messages.showInputDialog(project, "Enter issue title: ", IDE_ISSUER_TITLE, Messages.getQuestionIcon())
         var attempts = 0
 
-        while (title.isNullOrEmpty() && attempts < 5) {
+        if (title == null) // if he clicked cancel
+            return
+
+        var titleSize: Int? = title.length
+
+        val invalidLength = titleSize!! > ISSUE_TITLE_MAX_LENGTH
+        while (title!!.isEmpty() || invalidLength && attempts < 4) {
             title = Messages.showInputDialog(project, "Enter issue title: ", IDE_ISSUER_TITLE, Messages.getQuestionIcon())
+            titleSize = title?.length
+
+            if (invalidLength)
+                Messages.showErrorDialog(project, "Title is too long! $titleSize", IDE_ISSUER_TITLE)
+
             attempts++
         }
 
-        if (title.isNullOrEmpty()) {
-            Messages.showMessageDialog(project, "Too many attempts to open an issue. Try again later.", IDE_ISSUER_TITLE, Messages.getErrorIcon())
+        if (title.isEmpty() || invalidLength) {
+            Messages.showErrorDialog(project, "Too many attempts to open an issue. Try again later.", IDE_ISSUER_TITLE)
             return
         }
 
-        if (title.length > ISSUE_TITLE_MAX_LENGTH) {
-            Messages.showMessageDialog(project, "Title is too long! ${title.length}", IDE_ISSUER_TITLE, Messages.getErrorIcon())
+        val description = Messages.showInputDialog(project, "Enter description: ", IDE_ISSUER_TITLE, Messages.getQuestionIcon()) ?: return
+
+        val projectId = Messages.showInputDialog(project, "Enter project Id:  ", IDE_ISSUER_TITLE, Messages.getQuestionIcon()) ?: return
+        if (projectId.isEmpty()) {
+            Messages.showErrorDialog(project, "Project Id cannot be empty.", IDE_ISSUER_TITLE)
             return
         }
 
-        Messages.showInfoMessage("Successfully opened issue: $title", IDE_ISSUER_TITLE)
-        sendRequestToOpenIssue(project)
+        val accessToken = Messages.showInputDialog(project, "Enter Access token: ", IDE_ISSUER_TITLE, Messages.getQuestionIcon()) ?: return
+        if (accessToken.isEmpty()) {
+            Messages.showErrorDialog(project, "Access token Id cannot be empty.", IDE_ISSUER_TITLE)
+            return
+        }
+
+        createIssue(project, title, description, projectId, accessToken)
     }
 
-    private fun sendRequestToOpenIssue(project: Project) {
-        val url = URL("https://www.google.com/") // todo get git remote
+    private fun createIssue(project: Project, title: String, description: String, projectId: String, accessToken: String) {
+        val url = "https://gitlab.com/api/v4/projects/$projectId/issues"
+        val json = """
+        {
+            "title": "$title",
+            "description": "$description"
+        }
+        """.trimIndent()
 
-        val openConnection = url.openConnection() as HttpURLConnection
 
+        val body: RequestBody = json.toRequestBody(JSON)
+        val request: Request = Request.Builder()
+            .url(url)
+            .post(body)
+            .header("PRIVATE-TOKEN", accessToken)
+            .build()
+
+        val okHttpClient = OkHttpClient()
+
+        val response = try {
+            okHttpClient.newCall(request).execute()
+        } catch (e: Exception) {
+            throw RuntimeException("Error occurred while trying to open an issue.", e)
+        }
+
+        if (response.isSuccessful)
+            Messages.showMessageDialog("Opened issue: $title", IDE_ISSUER_TITLE, Messages.getInformationIcon())
+        else
+            Messages.showErrorDialog(project, "${response.message} ${response.code}", IDE_ISSUER_TITLE)
+    }
+
+    private fun getProjectId(project: Project): String {
+        val url = "https://gitlab.com/api/v4/projects/${project.name}"
+
+        val request: Request = Request.Builder()
+            .url(url)
+            .get()
+            .header("PRIVATE-TOKEN", "glpat-rJ4318uxDkcfnPFD8kqs")
+            .build()
+
+        val okHttpClient = OkHttpClient()
+
+        val response = try {
+            okHttpClient.newCall(request).execute()
+        } catch (e: Exception) {
+            throw RuntimeException("Error occurred while trying to open an issue.", e)
+        }
+
+        return response.body!!.string()
     }
 
     override fun update(event: AnActionEvent) {
@@ -65,18 +135,7 @@ class OpenIssueAction : AnAction() {
 
     private fun canOpenIssue(editor: Editor): Boolean {
         val line = getTextFromLine(editor)
-
         return line.contains("todo", ignoreCase = true)
-    }
-
-    private fun getCurrentRemote(project: Project): GitRemoteUrl {
-        // todo implement logic using GitRepositoryManager.getInstance(project)
-        return ""
-    }
-
-    private fun getRemote(): GitRemoteUrl {
-
-        return ""
     }
 
     override fun getActionUpdateThread(): ActionUpdateThread {
